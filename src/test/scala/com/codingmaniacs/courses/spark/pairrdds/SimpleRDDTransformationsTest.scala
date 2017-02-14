8137bd67f498dea209ed982991d9f604715ab52a
Geektimus <geektimus@users.noreply.github.com>
2017-02-13 21:17:19 -0500

    Fixed regex error, added groupByKey and combineByKey examples

--- src/test/scala/com/codingmaniacs/courses/spark/pairrdds/SimpleRDDTransformationsTest.scala
+++ src/test/scala/com/codingmaniacs/courses/spark/pairrdds/SimpleRDDTransformationsTest.scala
@@ -3,19 +3,20 @@ package com.codingmaniacs.courses.spark.pairrdds
 import java.util.Locale
 
 import com.holdenkarau.spark.testing.SharedSparkContext
-import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalatest.{FunSuite, Matchers}
 import org.slf4j.{Logger, LoggerFactory}
 
 /**
   * This class contains all the tests related to single RDD over Single RDD Transformations.
   */
-class SimpleRDDTransformationsTest extends FunSuite with SharedSparkContext {
+class SimpleRDDTransformationsTest extends FunSuite with SharedSparkContext with Matchers {
 
   @transient
   lazy val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   val extract: String =
-    """Bilbo drew his hand over his eyes. I am sorry, he said. But I felt so
+    """
+      Bilbo drew his hand over his eyes. I am sorry, he said. But I felt so
       queer. And yet it would be a relief in a way not to be bothered with it any
       more. It has been so growing on my mind lately. Sometimes I have felt it was
       like an eye looking at me. And I am always wanting to put it on and
@@ -29,7 +30,7 @@ class SimpleRDDTransformationsTest extends FunSuite with SharedSparkContext {
 
     val reduceBy = extractRDD
       .flatMap(lines => lines.toLowerCase(Locale.ENGLISH).split(" "))
-      .map(word => word.replaceAll("[;.,]", ""))
+      .map(word => word.replaceAll("[;.,\n\r]", ""))
       .filter(x => x.length > 8)
       .map(word => (word, 1))
       .reduceByKey((x, y) => x + y)
@@ -46,11 +47,52 @@ class SimpleRDDTransformationsTest extends FunSuite with SharedSparkContext {
 
     val groupByKey = extractRDD
       .flatMap(lines => lines.toLowerCase(Locale.ENGLISH).split(" "))
-      .map(word => word.replaceAll("[;.,]", ""))
+      .map(word => word.replaceAll("[;.,\n\r]", ""))
+      .filter(word => word.length > 1)
       .map(word => (word, 1))
       .groupByKey()
-      .
+      .map(x => (x._1, x._2.size))
+      .filter(x => x._2 > 3)
+      .collectAsMap()
 
-    groupByKey.foreach(x => x._2.foreach(println))
+    val expected = Map("and" -> 5, "to" -> 4, "it" -> 9)
+
+    assert(groupByKey == expected)
   }
+
+  test("basic combineByKey transformation") {
+    type ScoreCollector = (Int, Double)
+    type PersonScores = (String, (Int, Double))
+
+    val Eps = 1e-3
+
+    val initialScores = Array(("Fred", 88.0), ("Fred", 95.0), ("Fred", 91.0), ("Wilma", 93.0), ("Wilma", 95.0), ("Wilma", 98.0))
+
+    val wilmaAndFredScores = sc.parallelize(initialScores).cache()
+
+    val createScoreCombiner = (score: Double) => (1, score)
+
+    val scoreCombiner = (collector: ScoreCollector, score: Double) => {
+      val (numberScores, totalScore) = collector
+      (numberScores + 1, totalScore + score)
+    }
+
+    val scoreMerger = (collector1: ScoreCollector, collector2: ScoreCollector) => {
+      val (numScores1, totalScore1) = collector1
+      val (numScores2, totalScore2) = collector2
+      (numScores1 + numScores2, totalScore1 + totalScore2)
+    }
+    val scores = wilmaAndFredScores.combineByKey(createScoreCombiner, scoreCombiner, scoreMerger)
+
+    val averagingFunction = (personScore: PersonScores) => {
+      val (name, (numberScores, totalScore)) = personScore
+      (name, totalScore / numberScores)
+    }
+
+    val averageScores = scores.collectAsMap().map(averagingFunction)
+
+    averageScores.getOrElse("Fred", 0.0) should be(91.333 +- Eps)
+    averageScores.getOrElse("Wilma", 0.0) should be(95.333 +- Eps)
+  }
+
 }
