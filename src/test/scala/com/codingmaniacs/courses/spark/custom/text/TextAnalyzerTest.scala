490615fa77e1aa587f92cc454d725cfbf3016caa
Geektimus <alexander.kno+github@gmail.com>
2018-11-21 17:11:31 +0100

    Update dependencies, remove unused, add mew tests

--- src/test/scala/com/codingmaniacs/courses/spark/custom/text/TextAnalyzerTest.scala
+++ src/test/scala/com/codingmaniacs/courses/spark/custom/text/TextAnalyzerTest.scala
@@ -91,4 +91,28 @@ class TextAnalyzerTest extends FunSuite with SharedSparkContext with Matchers {
 
     assert(result.sameElements(expected))
   }
+
+  test("The text analyzer should group the lines by the regex and count the number of appearances matching the regex") {
+    val logLines: String =
+      """
+        |INFO: Dummy Info 1
+        |INFO: Dummy Info 2
+        |ERROR: Dummy Error 1
+        |INFO: Dummy Info 3
+        |INFO: Dummy Info 4
+        |ERROR: Dummy Error 2
+        |WARN: Dummy WARN 1
+        |INFO: Dummy Info 5
+      """.trim.stripMargin
+
+    val regex = """^.*:\s([a-zA-Z]+)\s.*""".r
+
+    val contentRDD = sc.parallelize(logLines.split("\n"))
+
+    val result = TextAnalyzer.countDataPerRowInText(regex, contentRDD)
+
+    val expected = Array(("Dummy", (8, 100)))
+
+    assert(result.sameElements(expected))
+  }
 }
